2b1e9b090907473bf3baa684b8dc74eb63e24f58
wnghks9457 <76986467+wnghks9457@users.noreply.github.com>
2021-01-12 10:51:57 +0900

    Create UIHpbar.cs

---
+++ UIHpbar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIHpbar;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField]
+    private Slider hpbar;
+
+    private float maxHp = 100;
+    private float curHp = 100;
+    float imsi;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpbar.value = (float)curHp / (float)maxHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(curHp > 0)
+            {
+                curHp -= 10;
+            }
+            else
+            {
+                curHp = 0;
+            }
+            imsi = (float)curHp / (float)maxHp;
+        }
+
+        HandleHp();
+    }
+
+    private void HandleHp()
+    {
+        hpbar.value = Mathf.Lerp(hpbar.value, imsi, Time.deltaTime * 10);
+    }
+}
+
